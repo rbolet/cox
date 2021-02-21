57eaac383f6653568e6af64bfa382589ff61cdac
rbolet <rapha.bolet@gmail.com>
2021-02-20 18:58:45 -0800

    testing for proper id format

--- src/App.test.js
+++ src/App.test.js
@@ -9,3 +9,12 @@ test("DatasetId appears", async () => {
   expect(await screen.findByText("Dataset Id:")).toBeInTheDocument();
   expect(await screen.findByRole("note")).toBeInTheDocument();
 });
+
+test("DatasetId is valid id", async () => {
+  render(<App />);
+  fireEvent.click(screen.getByText("GO!"));
+
+  const datasetSpan = await screen.findByRole("note");
+  expect(typeof datasetSpan.textContent).toBe("string");
+  expect(datasetSpan.textContent.length).toEqual(11);
+});
